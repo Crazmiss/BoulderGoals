54a2fda1a88d9826303ac545b6a6f0677c3b250b
Michaela Spevacek <spemiki@gmail.com>
2019-12-03 14:46:40 +0100

    Added model helpers with ide_generate:models

--- app/BoulderGrade.php
+++ app/BoulderGrade.php
@@ -4,6 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\BoulderGrade
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGrade newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGrade newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGrade query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGrade whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BoulderGrade whereName($value)
+ * @mixin \Eloquent
+ */
 class BoulderGrade extends Model
 {
     public $timestamps = false;
